0357ce628e63c2a03109d85023e59edcaff5e8b2
Garth Fritz <garth.fritz@gmail.com>
2013-04-17 20:12:27 -0400

    Updated database on Linux side

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20121205160435) do
     t.string   "remember_token"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
