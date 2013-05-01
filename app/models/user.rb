c899f27be9ca3a1031d5de44b3152625ae202032
Garth Fritz <garth.fritz@gmail.com>
2013-04-30 23:47:11 -0400

    9.6.1 admin attribute not accessible GREEN

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email, :password, :password_confirmation, :admin
+	attr_accessible :name, :email, :password, :password_confirmation
 	has_secure_password
 
 	before_save { self.email.downcase! }	
