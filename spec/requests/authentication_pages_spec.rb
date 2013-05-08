940b49557a3283248c46b9e6e1d06898be566c92
Garth Fritz <garth.fritz@gmail.com>
2013-05-08 12:24:13 -0400

    11.2.4 finished; completes follow/unfollow functionality; users can follow/unfollow by clicking buttons provided they are signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -88,6 +88,18 @@ describe "Authentication" do
 				end
 			end
 
+			describe "in the Relationships controller" do
+				describe "submitting to the create action" do
+					before { post relationships_path }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete relationship_path(1) }
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+
 			describe "in the Users controller" do
 				before { visit edit_user_path(user) }
 				it { should have_selector('title', text: 'Sign in') }
