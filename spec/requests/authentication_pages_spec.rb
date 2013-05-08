a262f3d0d1ec4b13c3d2dfa78582db31c342f0d4
Garth Fritz <garth.fritz@gmail.com>
2013-05-07 22:22:02 -0400

    10.3.1 finished; create and destroy micropost action requires users to be signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -91,7 +91,7 @@ describe "Authentication" do
 			describe "in the Users controller" do
 				before { visit edit_user_path(user) }
 				it { should have_selector('title', text: 'Sign in') }
-			
+
 				describe "visiting the user index" do
 					before { visit users_path }
 					it { should have_selector('title', text: 'Sign in') }
@@ -102,34 +102,50 @@ describe "Authentication" do
 				before { put user_path(user) }
 				specify { response.should redirect_to(signin_path) }
 			end
+
+			describe "in the Microposts controller" do
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { response.should redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before do
+						micropost = FactoryGirl.create(:micropost)
+						delete micropost_path(micropost)
+					end
+					specify { response.should redirect_to(signin_path) }
+
+				end
+			end
 		end
 
-		describe "as wrong user" do
-			let(:user) { FactoryGirl.create(:user) }
-			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-			before { sign_in user }
+			describe "as wrong user" do
+				let(:user) { FactoryGirl.create(:user) }
+				let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+				before { sign_in user }
 
-			describe "visiting Users#edit page" do
-				before { visit edit_user_path(wrong_user) }
-				it { should_not have_selector('title', text: full_title('Edit user')) }
+				describe "visiting Users#edit page" do
+					before { visit edit_user_path(wrong_user) }
+					it { should_not have_selector('title', text: full_title('Edit user')) }
+				end
+
+				describe "submitting a PUT request to the Users#update action" do
+					before { put user_path(wrong_user) }
+					specify { response.should redirect_to(root_path) }
+				end
 			end
 
-			describe "submitting a PUT request to the Users#update action" do
-				before { put user_path(wrong_user) }
-				specify { response.should redirect_to(root_path) }
-			end
-		end
+			describe "as non-admin user" do
+				let(:user) { FactoryGirl.create(:user) }
+				let(:non_admin) { FactoryGirl.create(:user) }
 
-		describe "as non-admin user" do
-			let(:user) { FactoryGirl.create(:user) }
-			let(:non_admin) { FactoryGirl.create(:user) }
+				before { sign_in non_admin }
 
-			before { sign_in non_admin }
-
-			describe "submitting a DELETE request to the Users#destroy action" do
-				before { delete user_path(user) }
-				specify { response.should redirect_to(root_path) }
+				describe "submitting a DELETE request to the Users#destroy action" do
+					before { delete user_path(user) }
+					specify { response.should redirect_to(root_path) }
+				end
 			end
 		end
 	end
-end
